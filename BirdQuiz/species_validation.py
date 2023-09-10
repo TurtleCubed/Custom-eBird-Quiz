b1f4e9119e7cbc753ceb1e16ca368ac1c37ff135
Tristan Yoo <tristan.yoo@berkeley.edu>
2023-09-09 19:01:08 -0700

    updated requirements.txt and added species_validation.py

---
+++ BirdQuiz/species_validation.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+from pandas import DataFrame
+from fast_autocomplete import AutoComplete
+import os
+
+CHECKLIST_URL = "https://com-aab-media.s3.amazonaws.com/common/ebird_taxonomy_v2022.csv"
+CHECKLIST_NAME = "ebird_taxonomy_v2022.csv"
+
+class Validate():
+
+    def __init__(self) -> None:
+        """Gets the 2022 taxononmy, and initalizes the validation set"""
+        if not os.path.isfile("BirdQuiz/resources/" + CHECKLIST_NAME):
+            self.get_checklist_from_url()
+        df = pd.read_csv("BirdQuiz/resources/" + CHECKLIST_NAME)
+        self.species_set = set(df["PRIMARY_COM_NAME"])
+
+    def get_checklist_from_url(self):
+        """Downloads the 2022 eBird taxonomy list to resources"""
+        with open(CHECKLIST_NAME, "wb") as f:
+            f.write(requests.get(CHECKLIST_URL).content)
+
+    def validate(self, name: str):
+        return name in self.species_set
+
+
+
+if __name__ == "__main__":
+    v = Validate()
+    print("\"Common Ostrich\" is valid: " + str(v.validate("Common Ostrich")))
+    print("\"Aaron Sun\" is valid: " + str(v.validate("Aaron Sun")))
